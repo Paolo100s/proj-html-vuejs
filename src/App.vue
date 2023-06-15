3b53caed32ad58d4c88ed2398fab00cac46b66d4
PaoloCentoscudi <paolocentoscudi@gmail.com>
2023-06-15 14:57:29 +0200

    add font

--- src/App.vue
+++ src/App.vue
@@ -36,4 +36,5 @@ export default {
 <style lang="scss">
 @use './styles/partials/variables.scss' as *;
 @use './styles/general.scss';
+@import url('https://fonts.googleapis.com/css2?family=Cabin&display=swap');
 </style>
